9d42af8313857734c4b8dffa37d98ff1fb0e8f9b
Marcin Luczak <mluczak92@gmail.com>
2021-07-04 13:35:15 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
